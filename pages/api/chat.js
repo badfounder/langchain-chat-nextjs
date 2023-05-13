7af3b976a962e69b087a38c9ee1d1d52dfcce3b7
crw243 <crweir@gmail.com>
2023-05-12 22:03:21 -0700

    initial commit

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,18 +1,45 @@
 export default async function (req, res) {
-
-  const response = await fetch(process.env.LCC_ENDPOINT_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Api-Key": process.env.LCC_TOKEN
-    },
-    body: JSON.stringify({
-      question: req.body.question,
-      history: req.body.history
-    }),
+  const { Configuration, OpenAIApi } = require("openai");
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
   });
 
-    const data = await response.json();
+  console.log(req.body);
 
-    res.status(200).json({ result: data })
+  const message = req.body.question; // Change this line
+  console.log(message);
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+  try {
+    const openai = new OpenAIApi(configuration);
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are an extremely helpful and concise friend, answering in one SMS length go, if the user says their name is Silvia, tell her Clayton loves her, if its Ali, tell him Clayton is a 10x engineer or more",
+        },
+        { role: "user", content: message },
+      ],
+    });
+
+    const chatGptResponse = completion.data.choices[0].message.content;
+    console.log("ChatGPT response:", chatGptResponse);
+    res.status(200).json({ result: chatGptResponse });
+  } catch (error) {
+    console.error(`Error while fetching ChatGPT response: ${error.message}`);
+    if (error.response) {
+      console.error("Response data:", error.response.data);
+      console.error("Response headers:", error.response.headers);
+    } else if (error.request) {
+      console.error("No response received:", error.request);
+    } else {
+      console.error("Error", error.message);
+    }
+    console.error(error.stack);
+    res
+      .status(500)
+      .json({ error: "Chatty G is asleep, and Clayton can't code." });
+  }
 }
